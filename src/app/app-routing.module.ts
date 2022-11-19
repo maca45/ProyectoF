822fbfd09955bd390b12c44f35b53ebba8f83ecd
iaraluna30 <iaraluna314@gmail.com>
2022-11-19 19:16:35 -0300

    Importe todos los componentes en el APP-ROUTING

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,32 @@ import { ContactoComponent } from './contacto/contacto.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ProductoComponent } from './producto/producto.component';
 
+//Importacion de productos
+import { AceitesComponent } from './productos/aceites/aceites.component';
+import { FrutosSecosComponent } from './productos/frutos-secos/frutos-secos.component';
+import { HarinasComponent } from './productos/harinas/harinas.component';
+import { LecheComponent } from './productos/leche/leche.component';
+import { MantecaComponent } from './productos/manteca/manteca.component';
+import { SalsasComponent } from './productos/salsas/salsas.component';
+import { ShampooComponent } from './productos/shampoo/shampoo.component';
+import { YerbaComponent } from './productos/yerba/yerba.component';
+
 
 const routes: Routes = [
   {path:'inicio', component:InicioComponent},
   {path:'contacto', component:ContactoComponent},
   {path:'producto', component:ProductoComponent},
   {path:'admin', component:AdminComponent},
+
+  //Productos
+  {path:'aceites', component:AceitesComponent},
+  {path:'frutos-secos', component:FrutosSecosComponent},
+  {path:'harinas', component:HarinasComponent},
+  {path:'leche', component:LecheComponent},
+  {path:'manteca', component:MantecaComponent},
+  {path:'salsas', component:SalsasComponent},
+  {path:'shampoo', component:ShampooComponent},
+  {path:'yerba', component:YerbaComponent},
 ];
 
 @NgModule({
